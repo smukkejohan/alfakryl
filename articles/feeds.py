0beedf4c4d3af26b26ccf299abc2bc0c3baefc54
Johan Bichel Lindegaard <mrbichel@mrbichel-laptop.(none)>
2009-11-02 21:47:49 +0100

    basic rss feed added, latest articles and latest articles for section

---
+++ articles/feeds.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.syndication.feeds import Feed
+from articles.models import Article
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.syndication.feeds import FeedDoesNotExist
+
+class LatestArticles(Feed):
+    title = "alfakryl.dk seneste artikler"
+    link = "/"
+    description = "Seneste artikler på alfakryl.dk"
+
+    def items(self):
+        return Article.objects.published().order_by('-pub_date')[:12]
+
+class LatestArticlesBySection(Feed):
+    def get_object(self, bits):
+        if len(bits) != 1:
+            raise ObjectDoesNotExist
+        return Section.objects.get(title__exact=bits[0])
+    
+    def title(self, obj):
+        return "Alfakryl.dk: sektionen %s" % obj.title
+    
+    def link(self, obj):
+        if not obj:
+            raise FeedDoesNotExist
+        return obj.get_absolute_url()
+
+    def description(self, obj):
+            return "Seneste artikler i sektionen %s" % obj.title
+            
+    def items(self):
+        return Article.objects.published().filter(sections=obj).order_by('-pub_date')[:12]
+
+
--- urls.py
+++ urls.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls.defaults import *
-
 from django.conf import settings
-
 from django.contrib import admin
 admin.autodiscover()
+from articles.feeds import LatestArticles, LatestArticlesBySection
+
+feeds = {
+    'latest': LatestArticles,
+    'sections': LatestArticlesBySection,
+}
 
 urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
-    
     (r'^admin/(.*)', admin.site.root),
-    (r'^photos/', include('photologue.urls')),
-    
+    (r'^photos/', include('photologue.urls')),   
     (r'^comments/', include('django.contrib.comments.urls')),
-
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+    (r'^accounts/', include('registration.urls')),
+    (r'^p/', include('accounts.urls')),
+    (r'^a/', include('alfakryl.articles.urls')),
     url(r'^$', 'views.index', name='main_index'),
     url(r'^redaktionen/$', 'views.redaktionen', name='redaktionen'),
     url(r'^dashboard/$', 'views.dashboard', name='dashboard'),
-    (r'^a/', include('alfakryl.articles.urls')),
-    (r'^p/', include('accounts.urls')),
-    (r'^accounts/', include('registration.urls')),
 
     #url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', 
         #{'sitemaps': sitemaps}, name='sitemap'),
