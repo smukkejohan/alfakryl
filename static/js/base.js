d6d49af7f1d60a07b2a46e55e7120faa350bf473
Johan Bichel Lindegaard <mrbichel@mrbichel-laptop.(none)>
2009-11-13 17:12:31 +0100

    slideshow jquery cycle

--- static/js/base.js
+++ static/js/base.js
@@ -2,16 +2,8 @@ google.load("jquery", "1.2");
 google.load("jqueryui", "1.5");
 
 google.setOnLoadCallback(function() {  
-        
     function onBefore() { 
         $('#phototitle')
             .html(this.alt); 
     };
-    
-    $('#articleslideshow').cycle({ 
-        delay:  2000, 
-        speed:  500, 
-        before: onBefore 
-    }); 
- 
 });
