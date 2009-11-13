f82654afba8c40f7dee2122b2e6238f6b4f2a2f5
Johan Bichel Lindegaard <mrbichel@mrbichel-laptop.(none)>
2009-11-13 15:44:06 +0100

    new crud form backend, and tag archive views working

---
+++ static/js/slug.js
@@ -0,0 +1,17 @@
+google.load("jquery", "1.2");
+
+google.setOnLoadCallback(function() {
+
+    $.fn.slugify = function(obj) {
+        $(this).data('obj', jQuery(obj));
+        $(this).keyup(function() {
+            var obj = $(this).data('obj');
+            var slug = $(this).val().replace(/\s+/g,'-').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
+            obj.val(slug);
+        });
+    }
+
+    $('#id_headline').slugify('#id_slug'); 
+    $('#id_title').slugify('#id_title_slug');
+
+});
