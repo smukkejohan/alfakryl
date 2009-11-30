b79e0fa46763484510fd33b4b7a955bd8924f0c8
Johan Bichel Lindegaard <mrbichel@mrbichel-laptop.(none)>
2009-11-30 21:51:52 +0100

    tag list for article detail view update with links to tag archive view

--- static/js/base.js
+++ static/js/base.js
@@ -4,7 +4,7 @@ google.load("jqueryui", "1.5");
 google.setOnLoadCallback(function() {
     
     $('#articleslideshow').cycle({ 
-        delay:  2000, 
+        delay:  3000, 
         speed:  500,
     }); 
  
